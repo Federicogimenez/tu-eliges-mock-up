c76aed41a732999ab34a0acd69c559e5c97389a1
fedegime <fgimenez@cuoma.com>
2023-11-08 12:10:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import './App.css'
+import './sass/main.scss'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        hi madafacars   
+        <br />
+        <br />
+        <br />
+        <img src="/src/assets/react.svg" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
