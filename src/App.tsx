f84d27c4a4009a9d609a4c4ad492f7da555b9457
fedegime <fgimenez@cuoma.com>
2024-01-12 16:35:21 -0300

    add redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import ThanksYou from "./pages/ThanksYou";
 import Faqs from "./pages/Faqs";
@@ -17,6 +17,9 @@ function App() {
           <Route path="/faqs" element={<Faqs />} />
           <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
           <Route path="/policy" element={<Policy />} />
+          
+          {/* redirect path does not exist */}
+          <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
       </LanguageProvider>
     </BrowserRouter>
