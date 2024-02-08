b64c876313ed4c5166caa89b5e23f479e6873f9e
fedegime <fgimenez@cuoma.com>
2024-02-08 01:46:14 -0300

    update video

--- src/layout/SectionVideo.tsx
+++ src/layout/SectionVideo.tsx
@@ -10,14 +10,15 @@ export default function SectionVideo() {
             <p>{ data["video_subtitle"] } </p>
 
             <div className="section-video__video-box">
-                <div style={{'padding':'56.25% 0 0 0','position':'relative'}}>
-                    <iframe 
-                        src="https://player.vimeo.com/video/898604407?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
-                        frameBorder={0}
-                        allow="autoplay; fullscreen; picture-in-picture" 
-                        style={{"position":"absolute","top":"0","left":"0","width":"100%","height":"100%"}} 
-                        title="TuEliges - Video Tour subtitulado Español"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-                </div>
+                    <div style={{'padding':'56.25% 0 0 0','position':'relative'}}>
+                        <iframe src="https://player.vimeo.com/video/904998093?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+                            frameBorder={0}
+                            allow="autoplay; fullscreen; picture-in-picture" 
+                            style={{"position":"absolute","top":"0","left":"0","width":"100%","height":"100%"}}
+                            title="Your Discount Program Website - Take the Tour"></iframe>
+                            </div>
+                            <script src="https://player.vimeo.com/api/player.js"></script>
+                    </div>
         </section>
     )
 }
