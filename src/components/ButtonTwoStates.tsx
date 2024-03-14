13febded5a9dc614d7af855e35a5a12ac3272a71
fedegime <fgimenez@cuoma.com>
2024-03-14 14:33:23 -0300

    update url recurly for uchooseit

--- src/components/ButtonTwoStates.tsx
+++ src/components/ButtonTwoStates.tsx
@@ -4,7 +4,7 @@ export default function ButtonTwoStates(options: ButtonTwoStatesInterface) {
   return (
     <>
         <a 
-          href="https://tueliges-us.recurly.com/subscribe/tueliges_member?currency=USD" 
+          href="https://uchooseit-us.recurly.com/subscribe/uchooseit_member?currency=USD" 
           className="btn-two-states">
             <span className="btn-two-states__visible"> { options.text_static } </span>
             <span className="btn-two-states__invisible"> { options.text_hover } </span>
