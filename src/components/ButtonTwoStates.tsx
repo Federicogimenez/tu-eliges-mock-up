2e72c3552e9fdf1b6aad0b50bef1f2443ef74b05
fedegime <fgimenez@cuoma.com>
2024-07-23 17:34:50 -0300

    update links redirect recurly and enjoy my deals

--- src/components/ButtonTwoStates.tsx
+++ src/components/ButtonTwoStates.tsx
@@ -6,14 +6,15 @@ export default function ButtonTwoStates({ animation='1', text_hover, text_static
     <>
       { animation == '1' &&
         <a 
-        href="https://uchooseit-us.recurly.com/subscribe/tueliges_member?currency=USD" 
+        target="_blank"
+        href="https://uchooseit-us.recurly.com/subscribe/uchooseit_member?currency=USD" 
         className="btn-two-states">
               <span className="btn-two-states__visible"> { text_static } </span>
               <span className="btn-two-states__invisible"> { text_hover } </span>
           </a>
       }
       { animation == '2' &&
-        <a 
+        <a target="_blank"
         href="https://uchooseitus.enjoymydeals.com/" 
         className="btn-two-states-horizontal">
               <span className="btn-two-states-horizontal__visible"> { text_static } </span>
