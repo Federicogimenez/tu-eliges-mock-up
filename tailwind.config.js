0af2a3a0be255482cfaacee9ca16be592d1834d2
fedegime <fgimenez@cuoma.com>
2024-11-20 14:40:32 -0300

    update thankyou content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      'lightblue-primary':'#2995FC',
+      'blue-primary':'#6166fd',
+      'pink-primary':'#DA82FF',
+    },
+    extend: {
+      fontFamily: {
+				montserrat: ['"Montserrat"', 'sans-serif'],
+				montserrat_italic: ['"Monserrat Italic"', 'sans-serif'],
+			},
+    },
   },
   plugins: [],
 }
