33cafb6c26b8b7f3de1ffe65bb383f7f0f8a3b5a
fedegime <fgimenez@cuoma.com>
2025-09-01 10:36:13 -0300

    update thankyou page and activate page

--- src/features/activate/Activate.tsx
+++ src/features/activate/Activate.tsx
@@ -37,30 +37,27 @@ export default function Activate() {
 
 
         <section>
-          <h1 className=" w-[90%] text-center text-[35px] md:text-[30px] mx-auto max-w-[800px] leading-[1.3] my-16 *:block *:uppercase *:text-[30px] *:md:text-[40px] animate-fade-in-1" >
+          <h1 className=" w-[90%] text-center text-[35px] md:text-[30px] mx-auto max-w-[800px] leading-[1.3] my-10 *:block *:uppercase *:text-[30px] *:md:text-[40px] animate-fade-in-1" >
             Here is the necessary information to
             <strong>
                 activate your membership.
             </strong>
           </h1>
-          <p className=" font-bold text-[20px] text-center animate-fade-in-1_5 *:text-blue-uchooseit *:mx-2" >
-
-          </p>
         </section>
         
-        <div className="w-[50%] lg:w-[30%] h-1 bg-blue-uchooseit max-w-[500px] mx-auto my-5 rounded-full" />
+        <div className="w-[50%] lg:w-[30%] h-1 bg-blue-uchooseit max-w-[300px] mx-auto mb-5 rounded-full" />
         
         <section className="relative  mx-auto w-[95%] max-w-[1000px] flex justify-center items-center gap-[10vw] py-8">
             <div onClick={()=>setInfo("web")} className={`w-fit flex flex-col gap-8 justify-center items-center cursor-pointer rounded-xl `}>
               <img src="/uchooseit-my-deals-web.png" alt="desktop" className={`portrait:h-[30vh] md:landscape:h-[40vh] object-contain transition-all duration-500 hover:scale-105 ${info == 'web' ? 'scale-105':'scale-100'} `} />
-              <button className={`cursor-pointer relative min-w-[100px] w-[40vw] md:w-[35vw] max-w-[400px] font-semibold border border-white text-[16px] md:text-[20px]  py-2 text-center rounded-full transition-all duration-300 hover:bg-white ${ info == "web" ? "bg-blue-uchooseit text-white" : "bg-white text-blue-uchooseit" }`}>
+              <button className={`cursor-pointer relative min-w-[100px] w-[40vw] md:w-[35vw] max-w-[400px] font-semibold border border-blue-uchooseit text-[16px] md:text-[20px]  py-2 text-center rounded-full transition-all duration-300  ${ info == "web" ? "bg-blue-uchooseit text-white" : " text-blue-uchooseit" }`}>
                 From Web
               </button>
             </div>
             <div onClick={()=>setInfo("mobile")} className="w-fit flex flex-col gap-8 justify-center items-center cursor-pointer ">
               <img src="/uchooseit-my-deals-mobile.png" alt="mobile" className={`portrait:h-[30vh] landscape:h-[40vh] object-contain transition-all duration-500 hover:scale-105 ${info == 'mobile' ? 'scale-105':'scale-100'}`} /> 
-              <button className={`cursor-pointer relative min-w-[100px] w-[40vw] md:w-[35vw] max-w-[400px] font-semibold border border-white text-[16px] md:text-[20px]  py-2 text-center rounded-full transition-all duration-300 hover:bg-white ${ info == "web" ? "bg-blue-uchooseit text-white" : "bg-white text-blue-uchooseit" }`}>
-                From Web
+              <button className={`cursor-pointer relative min-w-[100px] w-[40vw] md:w-[35vw] max-w-[400px] font-semibold border border-blue-uchooseit text-[16px] md:text-[20px]  py-2 text-center rounded-full transition-all duration-300 ${ info == "mobile" ? "bg-blue-uchooseit text-white" : " text-blue-uchooseit" }`}>
+                From Mobile
               </button>
             </div>
         </section>
@@ -120,7 +117,7 @@ export default function Activate() {
           <div className={`w-full overflow-hidden  ${ info == "mobile" ? "animate-fade-in z-30" : "animate-fade-out hidden"}`}>
             
             {/* <img src="/img/png/compras-2-banner.jpg" alt="bg-banner" className="absolute h-full w-full bottom-0 left-0 object-cover object-left opacity-40 " /> */}
-            <div className="absolute left-0 top-0 h-[70%] w-full object-contain bg-gradient-to-b from-white to-transparent" />
+            <div className="absolute left-0 top-0 h-[70%] w-full object-contain " />
 
             
             <div className="relative w-[90%] max-w-[800px] mx-auto my-6 h-fit p-4 text-center text-[18px] border-2 border-blue-uchooseit rounded-lg *:text-blue-uchooseit *:mx-2" >
@@ -145,14 +142,14 @@ export default function Activate() {
                 <div className="mb-1 pl-[3vw] ">
                   <p  className="text-left text-[18px] md:text-[20px] font-bold flex flex-nowrap justify-start items-center gap-4 ">
                     Organization Name:
-                    <strong className="relative font-montserrat_italic border-b-2 cursor-pointer border-b-blue-uchooseit bg-white px-[10px] text-[16px] md:text-[18px] rounded-md transition-all duration-500 hover:bg-blue-uchooseit hover:text-white " 
+                    <strong className="relative font-montserrat_italic border-b-2 cursor-pointer border-b-blue-uchooseit px-[10px] text-[16px] md:text-[18px] rounded-md transition-all duration-500 hover:bg-blue-uchooseit hover:text-white " 
                         >
                         uchooseit.us
                       </strong>
                   </p>
                   <p className="text-left text-[18px] md:text-[20px] font-bold  flex flex-nowrap justify-start items-center gap-4">
                       Member Id
-                      <strong className="relative mx-4 font-montserrat_italic border-b-2 cursor-pointer border-b-blue-uchooseit bg-white px-[10px] text-[16px] md:text-[18px] rounded-md transition-all duration-500 hover:bg-blue-uchooseit hover:text-white " 
+                      <strong className="relative mx-4 font-montserrat_italic border-b-2 cursor-pointer border-b-blue-uchooseit px-[10px] text-[16px] md:text-[18px] rounded-md transition-all duration-500 hover:bg-blue-uchooseit hover:text-white " 
                         ref={textAreaRef} 
                         onClick={copyToClipboard}>
                         {copySuccess == '' ? null : <small className="absolute left-full top-[-50%] bg-blue-uchooseit rounded-md text-white text-nowrap px-2 text-[14px] ">{copySuccess}</small>}
@@ -184,10 +181,10 @@ export default function Activate() {
 
             <div className="relative flex justify-center items-center gap-[5vw] mt-10 mb-20 w-[80%] md:max-w-[500px] mx-auto">
               <a href="https://play.google.com/store/apps/details?id=com.access.access&pli=1" target="_blank">
-                <img src="/img/png/android-logo.png" alt="" />
+                <img src="/android-logo.png" alt="" />
               </a>
               <a href="https://apps.apple.com/us/app/my-deals-mobile/id427118196" target="_blank">
-                <img src="/img/png/apple-logo.png" alt="" />
+                <img src="/apple-logo.png" alt="" />
               </a>
             </div>
 
--- src/features/thankyou/ThankYou.tsx
+++ src/features/thankyou/ThankYou.tsx
@@ -1,4 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
+// import MembershipCard from "../../shared/components/MembershipCard";
+// import TiltedCard from "../../shared/components/TiltedCard";
+import SpotlightCard from "../../shared/components/SpotlightCard";
 
 export default function ThankYou() {
 
@@ -7,7 +10,7 @@ export default function ThankYou() {
   const memberId = url.split('=').pop();
 
   return (
-    <div className="pt-[30dvh] text-center bg-gradient-to-b from-blue-uchooseit/50 to-transparent text-black dark:text-white">
+    <div className="pt-[25dvh] text-center bg-gradient-to-b from-blue-uchooseit/50 to-transparent text-black dark:text-white">
         <h1 className="text-4xl sm:text-5xl md:text-5xl xl:text-6xl shiny-blue-text text-center mx-auto block leading-[1.3]">
             Congratulations!
         </h1>
@@ -15,28 +18,40 @@ export default function ThankYou() {
             you are now part of our community!
         </p>
 
-        <p className="w-11/12 max-w-xl mx-auto mt-8 p-5 text-xl md:text-2xl xl:text-3xl mb-6 border border-white rounded-2xl shadow-lg shadow-white">
-            Your member ID is 
-            <strong className="mx-3 text-2xl md:text-3xl xl:text-4xl font-bold text-blue-uchooseit">
-                {memberId ? memberId : 'abc123'}
-            </strong>
-            <Link 
-                to="/activate"
-                className="uppercase w-[90%] max-w-[500px] mx-auto block mt-7 text-lg px-8 py-4 rounded-full border-2 border-black dark:border-white transition-all hover:font-semibold hover:bg-black hover:text-white hover:dark:bg-white hover:dark:text-black hover:-translate-y-1"
-                >
-                Activate Mambership
-            </Link>
+        <div className="flex justify-center mb-8 w-11/12 h-full max-w-lg mx-auto text-white">
+            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(225, 225, 255, 0.5)">
+                <img src="/uchooseit-white.svg" alt="logo" className="w-1/3 mx-auto" />
+                <h6 className="shiny-blue-text uppercase text-center text-xl font-bold mt-4">vip membership</h6>
+                <p className="w-full mx-auto text-xl md:text-2xl xl:text-2xl my-6">
+                    <strong className="mx-3 text-2xl md:text-3xl xl:text-4xl font-bold uppercase">
+                        {memberId ? memberId : 'abc123'}
+                    </strong>
+                </p>
+                <p className=" text-2xs">
+                    You choose where to save
+                </p>
+                <p className="mt-2 text-xs">
+                    Visual representation of your membership
+                </p>
+            </SpotlightCard>
+        </div>
+        
+        <Link 
+            to={"/activate?code="+memberId}
+            className="uppercase w-[90%] max-w-[500px] mx-auto block mt-7 text-lg px-8 py-4 rounded-full border-2 border-black dark:border-white transition-all hover:font-semibold hover:bg-black hover:text-white hover:dark:bg-white hover:dark:text-black hover:-translate-y-1"
+            >
+            Activate Mambership
+        </Link>
 
-        </p>
-        <p className="font-bold">
+        <p className="font-bold mt-8">
             We sent you an email from noreply@uchooseit.us
         </p>
-        <div className="w-full max-w-3xl mx-auto mt-2 mb-5  text-balance ">
+        <div className="w-full max-w-3xl mx-auto my-5  text-balance dark:text-gray-200 text-black/80 ">
             You have received everything you need to your email! Now you just have to activate the membership to start using it wherever you want.
         </div>
 
 
-        <a href="mailto:support@uchooseit.us" target="_blank" className="underline underline-offset-3">
+        <a href="mailto:support@uchooseit.us" target="_blank" className="underline text-gray-700 dark:text-gray-400 underline-offset-3">
             If you have questions, write to us at support@uchooseit.us
         </a>
     </div>
