37c350632c311c229b0ca7533a55fd8fe15a1041
fedegime <fgimenez@cuoma.com>
2025-11-12 15:30:09 -0300

    update calculate savings button color

--- src/shared/components/SavingsCalculator/CalculateSavingButton.tsx
+++ src/shared/components/SavingsCalculator/CalculateSavingButton.tsx
@@ -27,7 +27,7 @@ export default function CalculateSavingButton() {
           style={{
             animationDelay: '3s',
           }}
-          className={`group relative animate-fade cursor-pointer font-semibold leading-[1.2] pr-2 rounded-full w-40 flex justify-center items-center gap-1 transition-all duration-200 hover:bg-green-600 hover:scale-105 border text-white bg-green-400 px-2 py-1 ${ savingsBtn ? 'translate-x-3/5 hover:translate-x-3' : 'translate-x-5 hover:translate-x-3' }`}
+          className={`group relative animate-fade cursor-pointer font-semibold leading-[1.2] pr-2 rounded-full w-40 flex justify-center items-center gap-1 transition-all duration-200 hover:bg-black/80 hover:scale-105 border text-white bg-blue-uchooseit px-2 py-1 ${ savingsBtn ? 'translate-x-3/5 hover:translate-x-3' : 'translate-x-5 hover:translate-x-3' }`}
           >
             <img src="/icons/dollar.svg" alt="dollar" className="w-8" />
             <motion.span className={`transition-all ${ savingsBtn ? 'opacity-0 group-hover:opacity-100' : 'block'}`}
